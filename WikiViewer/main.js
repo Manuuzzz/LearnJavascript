28fdd932314bcf7ae8a651cbb792067af8ee51f5
Manuuzzz <emmanuel.lamon@gmail.com>
2018-05-14 20:44:32 +0200

    wikiSearch function is working

--- WikiViewer/main.js
+++ WikiViewer/main.js
@@ -10,10 +10,14 @@ add &origin=* for CORS issues or set requestheader
 
 const URL = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&list=search&utf8=1&srsearch=";
 const randomURL = "https://en.wikipedia.org/w/api.php?format=json&origin=*&action=query&generator=random&grnnamespace=0&prop=revisions&rvprop=content&grnlimit=1"
-
+const resultBaseURL = "http://en.wikipedia.org/?curid=";
+var searchResult = document.getElementById("searchResult");
+var insertPosition = document.getElementById("searchResult").parentNode;
 
 function init() {
-var input = document.getElementById("searchString");
+
+  
+ var input = document.getElementById("searchString");
 input.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
@@ -27,14 +31,34 @@ input.addEventListener("keyup", function(event) {
 function wikiSearch() {
  
 
-  var searchString = document.getElementById("searchString").value;
-  if(searchString == ""){
-  return; } else {
-  var modifiedURL = URL + searchString.replace(/\s/gi,"%20");
+    var searchString = document.getElementById("searchString").value;
+    if(searchString == ""){
+    return; } else {
+    var modifiedURL = URL + searchString.replace(/\s/gi,"%20");
 
-httpRequest(modifiedURL,function(response) {
+    httpRequest(modifiedURL,function(response) {
   
-    document.getElementById("showtest").innerHTML = response;
+          //clear results
+          searchResult.innerHTML = "";
+
+        for(var i = 0; i <9; i++) {
+        var jsonObj = JSON.parse(response);
+        var snippet = jsonObj.query.search[i].snippet;
+        var title = jsonObj.query.search[i].title;
+        var pageId = jsonObj.query.search[i].pageid;
+        resultURL = resultBaseURL + pageId;
+
+      
+
+        //add searchresults as li under UL
+
+        var Li = document.createElement("li");
+        Li.setAttribute("class","list-group-item");
+        Li.innerHTML = "<a href=\"" + resultURL + "\"target=\"_blank\" class=\"text-success\">" + "<h4>" + title + "</h4>" + snippet + "</a>";
+        searchResult.insertBefore(Li,searchResult.childNodes[0]);
+        }
+
+
   });
  
 }
@@ -66,8 +90,9 @@ function httpRequest(url,callback) {
         if (request.readyState == 4 && request.status == 200) {
             if (typeof callback === "function") {
                 
-             
+                            
                 return callback(request.responseText);
+              //  return callback(request.responseText);
         
         } else {  }
 
