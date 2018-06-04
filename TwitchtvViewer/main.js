7c51e74e16279e065feba940779c6e8074e381cf
Manuuzzz <emmanuel.lamon@gmail.com>
2018-06-04 20:38:10 +0200

    initial version - slider functionality

--- TwitchtvViewer/main.js
+++ TwitchtvViewer/main.js
@@ -13,25 +13,46 @@ https://dev.twitch.tv/docs/api/
 - use webhook to get the online/offline status of a user/stram/channel:
 https://dev.twitch.tv/docs/api/webhooks-reference/#topic-stream-updown
 
+todo: 
+- one function for the http requests, put url's in array? for each ..
+
+
 */
 
-const BASEURL_TWITCH = "https://api.twitch.tv/helix/users?login=esl_sc2&client_id=1w7pucg8bnt8is11cs3pl42ng41879";
+//const BASEURL_TWITCH = "https://api.twitch.tv/helix/users?login=esl_sc2&client_id=1w7pucg8bnt8is11cs3pl42ng41879";
+const BASEURL_TWITCH = "https://api.twitch.tv/helix/users?client_id=1w7pucg8bnt8is11cs3pl42ng41879&login=";
+const WEBHOOKURL_TWITCH = "https://api.twitch.tv/helix/streams?user_id=";
 
-
+var URL_ESL_SC2 = BASEURL_TWITCH + "esl_sc2";
+var userId_ESL_SC2 = "";
+var WEBHOOK_URL_ESL_SC2 = "";
         
 function init() {
-        httpRequest(BASEURL_TWITCH,function(response) {
+        
+    
+    httpRequest(URL_ESL_SC2,function(response) {
 
-        var jsonObj = JSON.stringify(response);
-        //var name = jsonObj.displayname;
-        alert(jsonObj);
-      //  document.getElementById("tester").innerHTML = name;
-      //  displayResult(response);   
+        var jsonObj = JSON.parse(response);
+        userId_ESL_SC2 = jsonObj.data[0].id;
+        WEBHOOK_URL_ESL_SC2 = WEBHOOKURL_TWITCH + userId_ESL_SC2;
+
+
+            httpRequest(WEBHOOK_URL_ESL_SC2,function(response) {
+
+                var jsonObj = JSON.parse(response);
+           
+               // alert(jsonObj.data[0].type);
+    
+             
+            });
+
          
-        });
+    });
  
+
+    
    
-    }
+}
 
 
 
@@ -62,4 +83,17 @@ function httpRequest(url,callback) {
   request.send();
 
 }
+$( document ).click(function() {
+    //$( "#toggle" ).toggle( "slide" , {direction:"right",distance:200}, testit());
+            
+           // $( "#toggle_esl_sc2" ).animate( {width: "toggle"}, "slow");
+           $( "#toggle_esl_sc2" ).animate( {width: "toggle"}, "slow");
+  });
 
+
+function testit()   {
+
+
+//$("#testertje").text("testertje");
+
+}
