fb145a3ab5806672c44e04232751be5d99f98b6a
Manuuzzz <emmanuel.lamon@gmail.com>
2018-10-16 22:35:05 +0200

    test bad pseudcode without randomization

--- JavascriptProgramming/Battleship/battleship.js
+++ JavascriptProgramming/Battleship/battleship.js
@@ -1,6 +1,6 @@
-var location1;
-var location2;
-var location3;
+var location1=1;
+var location2=2;
+var location3=3;
 
 var guess;
 var hits = 0;
@@ -14,7 +14,33 @@ guess = prompt("Guess a number from 1 to 6","");
 if (guess < 0 || guess > 6) {
 alert("Please enter a valid number");
 } else {
-guess = guess + 1;
+guesses = guesses + 1;
+switch(true) {
+case guess == location1:
+hits = hits +1;
+if (hits == 3) {
+    isSunk = true;
+    alert("You sank my battleship!");
+    }
+break;
+case guess == location2:
+hits = hits +1;
+if (hits == 3) {
+    isSunk = true;
+    alert("You sank my battleship!");
+    }
+break;
+case guess == location3:
+hits = hits +1;
+if (hits == 3) {
+    isSunk = true;
+    alert("You sank my battleship!");
+    }
+break;
+default:
+break;
+
+}
 }
 
 
